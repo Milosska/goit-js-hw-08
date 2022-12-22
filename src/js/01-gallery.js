003b9801943335ccd61f393d169424742f6d86d6
Milosska <110258611+Milosska@users.noreply.github.com>
2022-12-22 19:44:21 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,35 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+// Step 1 - HTML markup
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`
+  )
+  .join('');
+
+galleryEl.innerHTML = galleryMarkup;
+
+// Step 2 - "click" delegation
+galleryEl.addEventListener('click', onPreviewClick);
+
+function onPreviewClick(event) {
+  event.preventDefault();
+}
+
+// Step 3 - library use
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
